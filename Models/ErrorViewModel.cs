cbc8600cdffd554ef89ee079cd6bd360964c99b7
Wilson Kiggundu <wkiggundu@innovationvillage.co.ug>
2021-11-29 10:11:06 +0300

    Initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Treda.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
